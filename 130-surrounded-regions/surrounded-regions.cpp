7d70ccef0b25ff730bbe482417607b642747698f
Surya Kant Singh <surya53038@gmail.com>
2026-04-10 18:03:05 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (24.46%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -6,47 +6,45 @@ public:
 
         int dr[4] = {0,0,-1,1};
         int dc[4] = {-1,1,0,0};
-        
-        queue<pair<int,int>> q;
-        vector<vector<int>> grid(m, vector<int>(n, 0));
+        vector<vector<int>>grid(m, vector<int>(n));
+        queue<pair<int,int>>q;
 
-        // Step 1: push boundary O
         for(int i = 0; i < m; i++){
             for (int j = 0; j < n; j++){
                 if(i == 0 || i == m-1 || j == 0 || j == n-1){
                     if(board[i][j] == 'O'){
                         q.push({i,j});
-                        grid[i][j] = 1; // mark visited
+                        grid[i][j] = 1;
                     }
                 }
             }
         }
 
-        // Step 2: BFS (mark safe region)
+        
+
         while(!q.empty()){
-            auto temp = q.front();
-            q.pop();
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                pair<int,int>temp = q.front();
+                q.pop();
 
-            for(int k = 0; k < 4; k++){
-                int rr = temp.first + dr[k];
-                int cc = temp.second + dc[k];
+                for(int k = 0; k < 4; k++){
+                    int rr = dr[k] + temp.first;
+                    int cc = dc[k] + temp.second;
 
-                if(rr >= 0 && rr < m && cc >= 0 && cc < n 
-                   && board[rr][cc] == 'O' && grid[rr][cc] == 0){
-                    
-                    grid[rr][cc] = 1; // FIX
-                    q.push({rr,cc});
+                    if(rr >= 0 && rr < m && cc >= 0 && cc < n && board[rr][cc] == 'O' && grid[rr][cc] == 0){
+                        grid[rr][cc] = 1;
+                        q.push({rr,cc});
+                    }
                 }
             }
         }
-
-        // Step 3: convert surrounded regions
         for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(board[i][j] == 'O' && grid[i][j] == 0){
-                    board[i][j] = 'X';
+                for(int j = 0; j < n; j++){
+                    if(board[i][j] == 'O' && grid[i][j] == 0){
+                        board[i][j] = 'X';
+                    }
                 }
-            }
-        }
+            } 
     }
 };
