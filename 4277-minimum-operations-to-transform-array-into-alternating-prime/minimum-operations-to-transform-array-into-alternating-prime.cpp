7e991ec60a470559f829b42b787cf7fcca4cefde
Surya Kant Singh <surya53038@gmail.com>
2026-04-11 22:49:46 +0530

    Time: 300 ms (46.75%) | Memory: 116.5 MB (96.40%) - LeetSync

--- 4277-minimum-operations-to-transform-array-into-alternating-prime/minimum-operations-to-transform-array-into-alternating-prime.cpp
+++ 4277-minimum-operations-to-transform-array-into-alternating-prime/minimum-operations-to-transform-array-into-alternating-prime.cpp
@@ -8,28 +8,35 @@ public:
         return true;
     }
 
-    // int nearest_prime(int ele){
-    //     int d = 1;
-    //     while(true){
-    //         if(ele - d > 0 && isprime(ele-d)) return ele-d;
-    //         if(isprime(ele+d)) return ele+d;
-    //         d++;
-    //     }
-    // }
+    int nearest_prime(int ele){
+        int d = 1;
+        while(true){
+            // if(ele - d > 0 && isprime(ele-d)) return ele-d;
+            if(isprime(ele+d)) return ele+d;
+            d++;
+        }
+    }
+    int nearest_non_prime(int ele){
+        int d = 1;
+        while(true){
+            if(isprime(ele+d) == false) return ele+d;
+            d++;
+        }
+    }
     
     int minOperations(vector<int>& nums) {
         int ans = 0;
         for(int i = 0; i < nums.size(); i++){
             if(i % 2 == 0){
-                while(!isprime(nums[i])){
-                    nums[i]++;
-                    ans += 1;
+                if(!isprime(nums[i])){
+                    int nrst = nearest_prime(nums[i]);
+                    ans += abs(nrst - nums[i]);
                 }
             }
             else{
-                while(isprime(nums[i])){
-                    nums[i]++;
-                    ans += 1;
+                if(isprime(nums[i])){
+                    int nrst = nearest_non_prime(nums[i]);;
+                    ans += abs(nrst-nums[i]);
                 }
             }
         }  
