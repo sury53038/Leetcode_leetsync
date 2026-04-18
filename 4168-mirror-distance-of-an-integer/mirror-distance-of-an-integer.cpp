02f9729ee01f4748ad91e9ea4d21fb6ccace2ad1
Surya Kant Singh <surya53038@gmail.com>
2026-04-18 20:49:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (60.81%) - LeetSync

--- 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.cpp
+++ 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.cpp
@@ -3,9 +3,8 @@ public:
     int mirrorDistance(int n) {
         int num = n;
         int newNum = 0;
-        while(num){
-            int digit = num % 10;
-            newNum = newNum * 10 + digit;
+        while(num > 0){
+            newNum = newNum * 10 + (num % 10);
             num = num/10;
         }
         return abs(n-newNum);
